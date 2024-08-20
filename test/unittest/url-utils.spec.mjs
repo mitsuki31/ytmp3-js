6fb1ccd201dd33f4cec30145abafb57ce7f6bbb8
Ryuu Mitsuki <dhefam31@gmail.com>
2024-08-20 21:22:54 +0700

    test(url-utils): Add suite test for `url-utils` module

---
+++ test/unittest/url-utils.spec.mjs
@@ -0,0 +1,114 @@
+import assert from 'node:assert';
+
+import URLUtils from '../../lib/url-utils.js';
+
+describe('module:url-utils', function () {
+  describe('.URLUtils', function () {
+    const testMessages = {
+      constructor: [
+        'should throw an error when attempt to create new instance'
+      ],
+      extractVideoId: [
+        'should return the video ID of given YouTube URL if it is valid',
+        'should throw a `TypeError` if given URL is neither a string or URL object',
+        'should throw a `IDExtractorError` if an incorrect YouTube URL are given',
+        "should throw a `URIError` if the given URL's domain is not a YouTube domain"
+      ],
+      validateUrl: [
+        'should return true if the given URL is valid',
+        "should return false if the given URL's domain name is not a YouTube domain",
+        'should return false if the given URL is valid but with invalid video ID',
+        'should return true if the given URL is valid but with invalid video ID and `withId` disabled',
+        'should throw `TypeError` if the given URL is neither a string or URL object'
+      ],
+      validateId: [
+        'should return true if the given ID is valid',
+        'should return false if the given ID is invalid',
+        'should throw `TypeError` if the given ID is not a string'
+      ]
+    };
+
+    describe('#constructor', function () {
+      it(testMessages.constructor[0], function () {
+        assert.throws(() => URLUtils(), Error);
+        assert.throws(() => new URLUtils(), Error);
+      });
+    });
+
+    describe('#extractVideoId', function () {
+      const id = 'abcdeQWERTY';  // Valid video ID always have 11 characters
+      const url = `https://www.youtube.com/watch?v=${id}`;
+
+      it(testMessages.extractVideoId[0], function () {
+        assert.strictEqual(URLUtils.extractVideoId(url), id);
+      });
+
+      it(testMessages.extractVideoId[1], function () {
+        assert.throws(() => URLUtils.extractVideoId(123), TypeError);
+        assert.throws(() => URLUtils.extractVideoId([]), TypeError);
+        assert.throws(() => URLUtils.extractVideoId(true), TypeError);
+        assert.throws(() => URLUtils.extractVideoId(0n), TypeError);
+        assert.throws(() => URLUtils.extractVideoId(-Infinity), TypeError);
+      });
+
+      it(testMessages.extractVideoId[2], function () {
+        assert.throws(() =>
+          URLUtils.extractVideoId('https://youtu.be/watch?v=abc'), 'IDExtractorError');
+      });
+
+      it(testMessages.extractVideoId[3], function () {
+        assert.throws(() => URLUtils.extractVideoId('https://open.spotify.com'), URIError);
+      });
+    });
+
+    describe('#validateUrl', function () {
+      const exampleValidUrl = 'https://m.youtube.com/watch?v=12345-abcde';
+      const exampleInvalidUrl = 'https://youtu.be/12-=~56';
+
+      it(testMessages.validateUrl[0], function () {
+        assert.ok(URLUtils.validateUrl(exampleValidUrl));
+      });
+
+      it(testMessages.validateUrl[1], function () {
+        assert.equal(URLUtils.validateUrl('https://www.google.com/'), false);
+      });
+
+      it(testMessages.validateUrl[2], function () {
+        assert.equal(URLUtils.validateUrl(exampleInvalidUrl), false);
+      });
+
+      it(testMessages.validateUrl[3], function () {
+        assert.ok(URLUtils.validateUrl(exampleInvalidUrl, false));
+      });
+
+      it(testMessages.validateUrl[4], function () {
+        assert.throws(() => URLUtils.validateUrl(123), TypeError);
+        assert.throws(() => URLUtils.validateUrl([]), TypeError);
+        assert.throws(() => URLUtils.validateUrl(0n), TypeError);
+        assert.throws(() => URLUtils.validateUrl(-Infinity), TypeError);
+        assert.throws(() => URLUtils.validateUrl(/abc/), TypeError);
+      });
+    });
+
+    describe('#validateId', function () {
+      const exampleValidId = '_1234-zxcvO';  // Valid ID always have 11 characters
+      const exampleInvalidId = '123~V_';
+
+      it(testMessages.validateId[0], function () {
+        assert.ok(URLUtils.validateId(exampleValidId));
+      });
+
+      it(testMessages.validateId[1], function () {
+        assert.equal(URLUtils.validateId(exampleInvalidId), false);
+      });
+
+      it(testMessages.validateId[2], function () {
+        assert.throws(() => URLUtils.validateId(/_ba$/), TypeError);
+        assert.throws(() => URLUtils.validateId(0x12345), TypeError);
+        assert.throws(() => URLUtils.validateId(999n), TypeError);
+        assert.throws(() => URLUtils.validateId(Infinity), TypeError);
+        assert.throws(() => URLUtils.validateId({}), TypeError);
+      });
+    });
+  });
+});
