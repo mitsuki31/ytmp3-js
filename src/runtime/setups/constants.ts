29c522ed70c4ea61e592430bdc3f421b91a60c52
Ryuu Mitsuki <dhefam31@gmail.com>
2025-07-07 02:11:24 +0700

    feat(runtime): Add module utilities for setup funcs
    This includes:
      - `getStatus`: This function retrieves the current status of
         specific setup function. Returns `true` means the setup has
         been completed, `false` means not completed.
      - `setStatus`: This function sets the current status of
         specific setup function.

---
+++ src/runtime/setups/constants.ts
@@ -0,0 +1,61 @@
+/**
+ * Utilities for setup functions.
+ *
+ * @module    runtime/setups/constants
+ * @author    Ryuu Mitsuki <https://github.com/mitsuki31>
+ * @license   MIT
+ * @since     5.0.0
+ */
+
+import { YTMP3GlobalState } from '#globals';
+import { getGlob } from '#runtime/env';
+
+/** @private */
+export const MAX_LINE_LENGTH = 40;  // The maximum length of a log line
+
+/**
+ * Retrieves the current status of a setup function.
+ *
+ * @param name - The name of the setup function to query its status.
+ * @returns `true` if the setup function has been completed, `false` otherwise.
+ *
+ * @remarks
+ * This function is used to check if a setup function has been completed.
+ * It is useful for checking if certain setup functions have been completed
+ * before running another setup function that relies on the previous ones.
+ *
+ * @internal
+ * @since    5.0.0
+ */
+export function getStatus<T extends keyof NonNullable<YTMP3GlobalState["__setup"]>>(name: T): boolean {
+  const allStatus = getGlob('__setup');
+  if (!allStatus) return false;
+  return allStatus[name];
+}
+
+/**
+ * Sets the status of a setup function.
+ *
+ * @param name - The name of the setup function to set its status.
+ * @param value - The value to set the status to. If `true`, the setup
+ *                function is considered completed. Otherwise, the setup function is
+ *                considered not completed.
+ *
+ * @returns `true` if the status was successfully set, `false` otherwise.
+ *
+ * @remarks
+ * This function is used to set the status of a setup function. It is
+ * useful for setting the status of a setup function after it has been
+ * completed.
+ *
+ * @internal
+ * @since    5.0.0
+ */
+export function setStatus<
+  T extends keyof NonNullable<YTMP3GlobalState["__setup"]>
+>(name: T, value: boolean): boolean {
+  const allStatus = getGlob('__setup');
+  if (!allStatus) return false;
+  allStatus[name] = value;
+  return true;
+}
