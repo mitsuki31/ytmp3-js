4e2d52bcf03fd5725e6fe603913d65f14a790960
Ryuu Mitsuki <dhefam31@gmail.com>
2025-08-03 23:44:52 +0700

    refactor(core/internal): Minor refactor to `dlResult` module
    - Added a `acInfo.inputFileDeleted` parameter in `createDownloadResult`
      function
    - Added documentation to `AudioConversionResult` interface

--- src/core/internal/interfaces/AudioConversionResult.ts
+++ src/core/internal/interfaces/AudioConversionResult.ts
@@ -12,14 +12,24 @@ import type { FfprobeData } from 'fluent-ffmpeg';
  * @public
  */
 export default interface AudioConversionResult {
+  /** The input file information. */
   input: {
+    /** The path to the input audio file. */
     path: string;
+    /** The basename of the input audio file. */
     name: string;
+    /** Metadata of the input audio file, if available. Retrieved using FFprobe. */
     metadata?: FfprobeData;
+    /** Indicates if the input file was deleted after conversion. */
+    deleted: boolean;
   }
+  /** The output file information. */
   output: {
+    /** The path to the output audio file. */
     path: string;
+    /** The basename of the output audio file. */
     name: string;
+    /** Metadata of the output audio file, if available. Retrieved using FFprobe. */
     metadata?: FfprobeData;
   }
 }
